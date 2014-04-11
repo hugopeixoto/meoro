51eee0e3bdb28b3590e12567d58a3ddcb3aaf2e6
Pedro Coelho <pedro.luis.coelho@gmail.com>
2014-04-11 17:13:34 +0100

    Implemented basic bet -NOT WORKING-

--- webserver/config/routes.rb
+++ webserver/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show, :update]
+      resources :users, only: [:show, :update]
       resources :bets, only: [:index, :show, :create]
     end
   end
