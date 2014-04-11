c9665ea2bd40ed2e10d9ff868c31aa42adb5efe4
Hugo Peixoto <hugo.peixoto@ndrive.com>
2014-04-11 16:28:40 +0100

    root root

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get  '/topup', to: 'balance#index'
   post '/topup', to: 'balance#topup'
 
-  root 'welcome#index'
+  root 'balance#index'
 
   post '/balance/confirm', to: 'balance#confirm'
   post '/balance/cancel', to: 'balance#cancel'
